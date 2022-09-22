7dd3150d9c327bc6d64e494b69a87dc7627f765d
Jingfei Yao <sophiayao1990@gmail.com>
2022-09-22 10:06:23 -0400

    useState of meme object

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,12 +1,22 @@
+import { useState } from 'react'
 import '../App.css'
 import memesData from '../memesData'
 
 function Main() {
+    const [meme, setMeme] = useState({
+        topText: '',
+        bottomText: '',
+        randomImage:"http://i.imgflip.com/1bij.jpg"})
     function handleClick(){
         const memesArray = memesData.data.memes
         const randomNum = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNum].url
-        console.log(url)
+        setMeme(prevMeme => {
+            return{
+                ...prevMeme,
+                randomImage: memesArray[randomNum].url
+            }
+        })
+        // setMemeImage(memesArray[randomNum].url)
     }
     return(
         <main>
@@ -15,6 +25,7 @@ function Main() {
                 <input type="text" placeholder='bottom-msg' />
                 <button onClick={handleClick} >Get a new meme image 🖼</button>
             </div>
+            <img src={meme.randomImage} alt="memeImage" className='meme-image' />
         </main>
     )
 }
